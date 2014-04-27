e5291445f8a6c63a090a786b9ed427d5d4586ac3
jeffohrt <jeffohrt@gmail.com>
2014-04-28 05:31:33 +0700

    My attempt on Peer Assessment Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+## storing stuff with <<- operator
 
-## Write a short comment describing this function
+## creating storage object
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  ## wanted is what I am after - in this case - an inverse matrix
+  
+  wanted <- NULL
+  
+  set <- function(y){
+    
+    ## this is the fancy storage trick with <<-
+    
+    x <<- y
+    
+    wanted <<- NULL
+  }
+  
+  get <- function() x
+  
+  ## solve is the native R function for inverting matrices
+  
+  setsolve <- function(solve) wanted <<- solve
+  
+  getsolve <- function() wanted
+  
+  list(set=set, get=get, setsolve=setsolve, getsolve=getsolve)  
 
 }
 
 
-## Write a short comment describing this function
+## inverse exists or not, return inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## this grabs the stored copy if it is there
+  
+  wanted <- x$getsolve()
+  
+  ## if the inverse exists, return it
+  
+  if(!is.null(wanted)){
+    message("getting stored inverse")
+    return(wanted)
+  }
+  
+  ## otherwise calculate inverse, store it, return it
+  
+  data <- x$get()
+  
+  ## this is the resource hogging calculation
+  
+  wanted <- solve(data,...)
+  
+  ## this stores the inverse
+  
+  x$setsolve(wanted)
+  
+  ## return what we are after
+  
+  wanted
+  
 }
